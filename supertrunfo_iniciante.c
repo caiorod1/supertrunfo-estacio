69d1844a5c6cf595c2e4108c98a291bb219b55c0
Caio Rodrigues <caio.contato03@gmail.com>
2025-02-24 14:58:48 -0300

    Update and rename supertrunfo_main.c to supertrunfo_iniciante.c
    SuperTrunfo Iniciante

--- supertrunfo_iniciante.c
+++ supertrunfo_iniciante.c
@@ -4,11 +4,12 @@ int main(){
    /*
    Criando as variaveis para armazenar os cadastros das cartas do usuario e logo após mostrar os atributos que o mesmo adicinou
    */
-    char nome [50], nome2 [20], codigo[6], codigo2 [6]; // Nome para o estado e codigo da cidade
+    char nome[50], nome2[20], codigo[6], codigo2 [6]; // Nome para o estado e codigo da cidade
     double populacao, populacao2, PIB, PIB2;  // população da cidade //PIB da cidade 
     float area, area2;  // area da cidade //PIB da cidade 
     int turistico, turistico2; // pontos turisticos da cidade
-    
+    char estado[5], estado2[5];
+
 // Aprensetação do jogo.
 printf("Bem-Vindo ao jogo Super Trunfo Paises.\n");
 printf("Para iniciarmos o jogo, siga as instruções abaixo.\n");
@@ -17,58 +18,64 @@ printf("Para iniciarmos o jogo, siga as instruções abaixo.\n");
 printf("Coloque algumas informações da Carta 1: Nome da cidade, população, área, PIB e número de pontos turísticos.\n"); 
 
 printf("Insira o nome da primeira cidade: ");
-scanf(" %[^\n]s", &nome);
+scanf(" %[^\n]", nome);
 
-printf("Insira a população da cidade: ");
+printf("Insira a população: ");
 scanf("%lf", &populacao);
 
-printf("Insira a área da cidade: ");
+printf("Insira a área: ");
 scanf("%f", &area);
 
 printf("Insira o PIB: ");
 scanf("%lf", &PIB);
 
-printf("Existem quantos pontos turisticos na cidade: ");
+printf("Insira a quantidade de pontos turisticos: ");
 scanf("%d", &turistico);
 
-printf("Registre um codigo para carta com uma letra A - H e um numero de 01 - 04, seguindo este formato = A01.\n"); 
-printf("Insira o codigo da cidade: ");
-scanf("%s", &codigo);
+printf("Vamos representar o Estado com uma letra (A - H) e um código (01 - 04). \n"); 
+
+printf("Insira a uma letra (A - H): ");
+scanf("%s", estado);
+
+printf("Insira o codigo (01 - 04): ");
+scanf("%s", codigo);
 
 // Cadastrando a carta  2
-printf("Agora vamos cadastrar a Carta 2.\n");  
 
-printf("Coloque as informações da Carta 2: \n"); 
+printf("Agora vamos cadastrar a Carta 2.\n");  
 
 // Cadastrando a segunda carta
 printf("Insira o nome da segunda cidade: "); 
-scanf(" %[^\n]s", &nome2);
+scanf(" %[^\n]", nome2);
 
-printf("Insira a população da cidade: ");
+printf("Insira a população: ");
 scanf("%lf", &populacao2);
 
-printf("Insira a área da cidade: ");
+printf("Insira a área: ");
 scanf("%f", &area2);
 
 printf("Insira o PIB: ");
 scanf("%lf", &PIB2);
 
-printf("Existem quantos pontos turisticos na cidade: ");
+printf("Insira a quantidade de pontos turisticos: ");
 scanf("%d", &turistico2);
 
-// instrução como escrever o codigo para o usuario não colocar o mesmo codigo.
-printf("Seguindo o mesmo exemplo da CARTA 1, use uma letra (A-H) seguida de um número (01-04), ex: A02.\n"); 
-printf("OBS: Pode utilizar a mesma letra da carta 1, mas precisa colocar o numero diferente.\n");
-printf("Insira o codigo da segunda cidade: ");
-scanf("%s", &codigo2);
+printf("Insira o estado ( A - H ): ");
+scanf("%s", estado2);
+
+printf("Insira o codigo ( 01 - 04 ): ");
+scanf("%s", codigo2);
+
 
 // Depois de adicionar todas as informações, agora está na hora de apresentar!
 printf("Pronto, agora vou está te mostrando os atributos das duas cartas.\n"); 
 
 // Apresentação da carta 1
-printf("\nCARTA 1: \n");   
-printf("\tCódigo: %s\n", codigo);
-printf("\tNome da cidade: %s\n", nome);
+printf("\nCARTA 1: \n");
+
+printf("\tEstado: %s\n", estado);
+printf("\tCódigo: %s%s\n", estado, codigo);
+printf("\tCidade: %s\n", nome);
 printf("\tPopulação: %.0lf\n", populacao);
 printf("\tÁrea: %.2f km²\n", area);
 printf("\tPIB: %.2lf bilhões\n", PIB);
@@ -76,8 +83,10 @@ printf("\tPontos turisticos: %d\n", turistico);
 
 // Apresentação da carta 2
 printf("\nCARTA 2: \n");
-printf("\tCódigo: %s\n", codigo2);
-printf("\tNome da cidade: %s\n", nome2);
+
+printf("\tEstado: %s\n", estado2);
+printf("\tCódigo: %s%s\n", estado2, codigo2);
+printf("\tCidade: %s\n", nome2);
 printf("\tPopulação: %.0lf\n", populacao2);
 printf("\tÁrea: %.2f km²\n", area2);
 printf("\tPIB: %.2lf bilhões\n", PIB2);
